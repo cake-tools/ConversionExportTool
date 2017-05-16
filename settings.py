b5d6bd2694d971a04df5084679766a9a17979476
Paul Chung <paulchung39@gmail.com>
2017-05-15 21:13:08 -0700

    added unit tests for queue message and queue job functions

--- settings.py
+++ settings.py
@@ -1,20 +1,20 @@
-
 MONGODB_DATABASE = {
-    'uri': '', #example: mongodb://test:pass@ds15132128.mlab.com:19231/job_table
-    'database_name': '', #example: job_table
-    'collection_name': '' #example: job
+    'uri': 'mongodb://paul:test@ds151018.mlab.com:51018/job_table', #example: mongodb://test:pass@ds15132128.mlab.com:19231/job_table
+    'database_name': 'job_table', #example: job_table
+    'collection_name': 'job' #example: job
 }
 
 SQS_QUEUE = {
-    'name': '', #example: my_queue
-    'url': '' #example: https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue
+    'name': 'MyQueue', #example: my_queue
+    'url': 'https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue' #example: https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue
 }
 
 S3_BUCKET = {
-    'name': '' #example: exportreports
+    'name': 'conversionreports'
 }
 
-ADMIN_DOMAIN_URL = '' #DO NOT include 'http://' before the url  example: 'admin.lab.com' not 'http://admin.lab.com'
+ADMIN_DOMAIN_URL = 'login.affle.com'
 
-API_KEY = '' #Your Cake instance API key.
+APP_SECRET_KEY = 'somethingsecret'
 
+API_KEY = 'AF7O9BHpCxJUwnHGyflqF00LXYOM1gQb'
