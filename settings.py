02cacb77600d29e5ee9858c0cc26a9720e3754f3
Paul Chung <paulchung39@gmail.com>
2017-06-05 23:20:10 -0700

    Delete runtime.txt

---
+++ settings.py
@@ -0,0 +1,20 @@
+MONGODB_DATABASE = {
+    'uri': 'mongodb://paul:test@ds151018.mlab.com:51018/job_table', #example: mongodb://test:pass@ds15132128.mlab.com:19231/job_table
+    'database_name': 'job_table', #example: job_table
+    'collection_name': 'job' #example: job
+}
+
+SQS_QUEUE = {
+    'name': 'MyQueue', #example: my_queue
+    'url': 'https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue' #example: https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue
+}
+
+S3_BUCKET = {
+    'name': 'conversionreports'
+}
+
+ADMIN_DOMAIN_URL = 'login.affle.com'
+
+APP_SECRET_KEY = 'somethingsecret'
+
+API_KEY = 'AF7O9BHpCxJUwnHGyflqF00LXYOM1gQb'
