cfa4f93175dfd01885713945cd35c0117d5057ab
Paul Chung <paulchung39@gmail.com>
2017-06-06 00:15:38 -0700

    updated settings file

--- settings.py
+++ settings.py
@@ -1,20 +1,18 @@
 MONGODB_DATABASE = {
-    'uri': 'mongodb://paul:test@ds151018.mlab.com:51018/job_table', #example: mongodb://test:pass@ds15132128.mlab.com:19231/job_table
-    'database_name': 'job_table', #example: job_table
-    'collection_name': 'job' #example: job
+    'uri': '', #example: mongodb://test:pass@ds15132128.mlab.com:19231/job_table
+    'database_name': '', #example: job_table
+    'collection_name': '' #example: job
 }
 
 SQS_QUEUE = {
-    'name': 'MyQueue', #example: my_queue
-    'url': 'https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue' #example: https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue
+    'name': '', #example: my_queue
+    'url': '' #example: https://sqs.us-east-1.amazonaws.com/029737694246/MyQueue
 }
 
 S3_BUCKET = {
-    'name': 'conversionreports'
+    'name': ''
 }
 
-ADMIN_DOMAIN_URL = 'login.affle.com'
+ADMIN_DOMAIN_URL = ''
 
-APP_SECRET_KEY = 'somethingsecret'
-
-API_KEY = 'AF7O9BHpCxJUwnHGyflqF00LXYOM1gQb'
+API_KEY = ''
